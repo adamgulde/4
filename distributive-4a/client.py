b0d7787ad8f3b1d00880222691bb9851561efce9
adamgulde <adam.j.gulde@gmail.com>
2023-04-28 10:30:37 -0400

    working back and forth :)

--- distributive-4a/client.py
+++ distributive-4a/client.py
@@ -53,21 +53,21 @@ def config(credentials:tuple):
             time.sleep(3)
     return driver
 
-user_list = []
-def create_url_list(start_id:int, end_id:int):
-    for id in range(end_id - start_id):
-        base = f"https://bishopmoore.schoology.com/user/{start_id + id}/info"
-        user_list.append(base)
-    return user_list
+# user_list = []
+# def create_url_list(start_id:int, end_id:int):
+#     for id in range(end_id - start_id):
+#         base = start_id + id
+#         user_list.append(base)
+#     return user_list
 
 user_exists = []
-def get_positive_user_id(driver :webdriver, url_list :list):
+def get_positive_user_id(start_id:int, end_id:int, driver :webdriver):
     wait = WebDriverWait(driver, 0, poll_frequency=0.01)
-    for user in url_list:
-        driver.get(user)
+    for user in range(start_id, end_id+1):
+        driver.get("https://bishopmoore.schoology.com/user/"+str(user)+"/info")
         try:
             wait.until(EC.title_contains("|"))
-            user_exists.append(1) 
+            user_exists.append(user) 
         except TimeoutException:
             print("Unavailable user at: ", user)
     driver.quit()        
@@ -75,9 +75,9 @@ def get_positive_user_id(driver :webdriver, url_list :list):
 def client_main(cred, start_id, end_id):
     input(f"Press ENTER to iterate through {end_id-start_id} users...")
     d = config(cred)
-    user_list = create_url_list(start_id, end_id) # can be simplified to just use IDs.. 
+    # user_list = create_url_list(start_id, end_id) # can be simplified to just use IDs.. 
     start_time = time.time()
-    get_positive_user_id(d, user_list)
+    get_positive_user_id(start_id, end_id, d)
     end_time = time.time()
     efficiency = round((end_id-start_id)/(end_time-start_time), 3)
     print(f"Total time iterating: {(int(end_time-start_time))//60}m:{(int(end_time-start_time))%60}s\nTotal URLS iterated: {end_id-start_id}\nAverage runtime efficiency: {efficiency} users/sec")
@@ -85,21 +85,9 @@ def client_main(cred, start_id, end_id):
 
 
 def webscrape(ids):
-    # id1 = ''
-    # id2 = ''
-    # next = False
-    # for id in ids: ### Parsing string properly
-    #     if id != ',':
-    #         if not next:
-    #             id1 = id1 + id
-    #         if next:
-    #             id2 = id2 + id
-    #     else:
-    #         next = True
-    # print(id1, id2)
     creds = client_initialize()
     positive_ids = client_main(creds, int(ids[0]), ids[1])
-    print("[CLIENT] Finished finding users!\n\n")
+    print("[CLIENT] Finished finding users!")
     return positive_ids
 
 def sendToServer(positiveIDs:str):
@@ -107,16 +95,21 @@ def sendToServer(positiveIDs:str):
     # for id in positiveIDs:
     #     file.write(id+'\n')
     # file.close()
-    client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    client_socket.connect((ip, port))
     
-    client_socket.send(positiveIDs.encode())
+    print(positiveIDs)
+    strIDs = ' '.join(str(id) for id in positiveIDs)
+    print(strIDs)
+    input()
+
+    client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    client_socket.connect((ip, port+1))
+    client_socket.send(strIDs.encode())
+    print('[CLIENT] Sent positive ID data!')
 
 
 def get_command():
     client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     client_socket.connect((ip, port))
-    # client_socket.send('0'.encode())
     command = client_socket.recv(20).decode().strip().split(',')
     conv_cmd = int(command[0]), int(command[1])
     print('[CLIENT] Received command!')
@@ -124,18 +117,14 @@ def get_command():
     client_socket.close()
     return conv_cmd
 
-###
-###
-###
 
 def main():
     ids = get_command()
     posIDS = webscrape(ids)
-    # posIDs = prune('distributive-4a/'+filename)
     sendToServer(posIDS)
     import webbrowser
-    webbrowser.open('https://youtu.be/G1IbRujko-A')
-    print('\n[CLIENT] Client script completed!') ## this never gets reached but I am so sick of javascript at this point that I do not care
+    webbrowser.open('https://youtu.be/BBGEG21CGo0')
+    print('[CLIENT] Client script completed!') 
 
 ip, port = input('Enter IPv4 Address of Master: '), int(input('Enter Port # '))
 if __name__=="__main__":
--- distributive-4a/server.py
+++ distributive-4a/server.py
@@ -3,27 +3,37 @@ from _thread import *
 import threading
 from sys import platform
 
-# Thread to handle each "client_soc" connection
-def handler_master(client_soc:socket, client_add, command_list:list):
-    print(f"[MASTER THREAD] Connection request accepted from {client_add[0]},{client_add[1]}")
-    client_soc.send(command_list.pop(0).encode())
-    client_soc.close()
+directory = 'distributive-4a'
+if(platform.startswith('win32')):
+    directory = 'distributive-4a\\'
+else:
+    directory = 'distributive-4a/'
 
-def handler_receiver(client_soc:socket, client_add):
+# Thread to handle each "client_soc" connection
+def handler_master(client_soc:socket, client_add, command_list:list[str]):
     print(f"[MASTER THREAD] Connection request accepted from {client_add[0]},{client_add[1]}")
-    client_soc.socket.recvmsg()
+    cmd = command_list.pop(0)
+    client_soc.send(cmd.encode())
+    print(f"[MASTER THREAD] Sent commands of {cmd} to {client_add[0]}, {client_add[1]}")
     client_soc.close()
+    print(f"[MASTER THREAD] Closed server-to-client socket on {client_add[0]}, {client_add[1]}")
+
+# Thread to handle each receiving data connection
+def handler_receiver(client_soc:socket, client_add):
+    print(f"[RECEIVER THREAD] Connection request accepted from {client_add[0]},{client_add[1]}")
+    data = client_soc.recv(1000)
+    
+    while data:
+        print(data.decode())
+        data = client_soc.recv(1000)
+    client_soc.close()
+    print(f"[RECEIVER THREAD] Closed client-to-server socket on {client_add[0]},{client_add[1]}")
+
 
 def main_master(port:int):
+    print("[MASTER] Thread started!")
     commands = None
-    if(platform.startswith('win32')):
-        with open('distributive-4a\cmds.txt', 'r') as cmd: 
-            commands = cmd.readlines()
-            commands.pop(0)
-            cmd.close()
-        ## create commands list
-    else:
-        with open('distributive-4a/cmds.txt', 'r') as cmd:
+    with open(directory+'cmds.txt', 'r') as cmd: 
             commands = cmd.readlines()
             commands.pop(0)
             cmd.close()
@@ -33,34 +43,32 @@ def main_master(port:int):
         server_socket.listen()
         print('[MASTER] Master accepting connections...')
         while(True):
-            if(platform.startswith('win32')):
-                with open('distributive-4a\cmds.txt', 'r') as cmd: 
-                    client_connected, client_address = server_socket.accept()
-                    threading.Thread(target=handler_master,args=(client_connected, client_address, commands), daemon=True).start() 
-                    
-                    if cmd.readline() == 'stop':
-                        print('[SERVER] Server stopped!')
-                        break
-                ## create commands list
-            else:
-                with open('distributive-4a/cmds.txt', 'r') as cmd:
-                    client_connected, client_address = server_socket.accept()
-                    threading.Thread(target=handler_master,args=(client_connected, client_address, commands), daemon=True).start() 
-                    
-                    if cmd.readline() == 'stop':
-                        print('[SERVER] Server stopped!')
-                        break                
+            with open(directory+'cmds.txt', 'r') as cmd: 
+                client_connected, client_address = server_socket.accept()
+                threading.Thread(target=handler_master,args=(client_connected, client_address, commands), daemon=True).start() 
+                
+                if cmd.readline() == 'stop':
+                    print('[SERVER] Server stopped!')
+                    break            
     server_socket.close()
+
 def main_receiver(port:int):
-    all_pids = ''
     print('[RECEIVER] Thread started!')
     with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as server_socket: 
         server_socket.bind(('', port))
         server_socket.listen()
         print('[RECEIVER] Receiver accepting connections...')
+        
+        ### OS conditional
+        
+        client_connected, client_address = server_socket.accept()
+        threading.Thread(target=handler_receiver,args=(client_connected, client_address), daemon=True).start() 
+        
+        ### end OS conditional 
+    server_socket.close()
 
 if __name__ == "__main__":
     port = int(input('Enter port # '))
     # main_master()
     threading.Thread(target=main_master, args=[port]).start()
-    threading.Thread(target=main_receiver, args=[port]).start()
+    threading.Thread(target=main_receiver, args=[port+1]).start()
